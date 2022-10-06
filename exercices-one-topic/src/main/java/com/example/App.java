3378409bbae4b076080f656e8da46343e02d99a1
Luis Emilio Grijalba <luisegrijalba8@gmail.com>
2022-10-05 19:46:56 -0500

    excercices one topic

---
+++ exercices-one-topic/src/main/java/com/example/App.java
@@ -0,0 +1,35 @@
+package com.example;
+
+public final class App {
+    public static void main(String[] args) {
+
+        // topic one exercises
+
+        // number
+        byte number = 127;
+        short numberOne = 32767;
+        int numberTwo = 999999999;
+        long numberThree = 11234567892345641L;
+
+        float numberFloatOne = 21.522F;
+        double numberFloatTwo = 214.23454594794759674;
+
+        // chraracter
+
+        char character = 'A';
+
+        // string
+
+        String stringOne = "hello, world!";
+
+        System.out.println("byte is: " + number);
+        System.out.println("short is: " + numberOne);
+        System.out.println("int is: " + numberTwo);
+        System.out.println("long is: " + numberThree);
+        System.out.println("float is: " + numberFloatOne);
+        System.out.println("double is: " + numberFloatTwo);
+        System.out.println("char is: " + character);
+        System.out.println("String is: " + stringOne);
+
+    }
+}
